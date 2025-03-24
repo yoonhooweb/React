44f703e0b39de984d6addf1c39e454ea73bea32a
joyoonhoo <showil20@naver.com>
2025-03-24 10:26:03 +0900

    c

--- react-function-app/src/App.jsx
+++ react-function-app/src/App.jsx
@@ -1,15 +1,105 @@
+import { useState } from 'react';
 import './App.css'
 
 /*
-	1. 제목데이터 뿌리기
-	2. 좋아요 기능 만들기
+	1. 제목데이터 뿌리기 o
+	2. 좋아요 기능 만들기 o
 	3. 모달창 만들기 -> 각 모달별로 제목 다르게 뜨게하기
-	4. 버튼누르면 글자 변경
-	5. 정렬기능능
+	4. 버튼누르면 글자 변경 o
+	5. 정렬기능능 o
 */
 
 function App() {
 
+	//제목데이터
+	let [fruitName, setFruitName] = useState(['d','w','a','c']);
+	// 좋아요 카운팅
+	let [like, setLike] = useState([0,0,0,0]);
+	let [isModal , setIsModal] = useState(false);
+	let [modalTitle , setModalTitle] = useState('');
+
+
+	function likeBtn(i) {
+		let newLike = [...like];
+		newLike[i]++;
+		setLike(newLike);
+		console.log(newLike);
+	}
+
+	function fruitNameReset() {
+		let newName = [...fruitName];
+		newName[0] = '딸기';
+		setFruitName(newName);
+	}
+
+	function fruitSort () {
+		let newSort = [...fruitName];
+		newSort = newSort.sort();
+		setFruitName(newSort);
+	}	
+
+	const modalOpen = (i) => {
+		setIsModal(true);
+		setModalTitle(fruitName[i]);
+	}
+
+	const arrayPush = () => {
+
+		let fruitInput = document.querySelector("#fruitInput")
+
+		let pushArray = [...fruitName];
+		pushArray.push(fruitInput.value)
+		fruitInput.value = '';
+		console.log(pushArray);
+		setFruitName(pushArray);
+
+	}
+
+	const arrayRemove = (i) => {
+		let removeArray = [...fruitName];
+		removeArray[i] = removeArray.splice(i,0);
+		setFruitName(removeArray)
+	}
+
+	return (
+		<>
+			<h3 className='title'>fruit</h3>
+			<ul className='list'>
+				{
+					fruitName.map( ( v, i) => {
+						return (
+							<li key={v}>
+								<span>{v}</span>
+								<button className='like' onClick={ ()=> likeBtn(i) }> 👍 </button>
+								<span className='like-count'> {like[i]} </span>
+								<button onClick={ () => modalOpen(i) }>모달</button>
+								<button onClick={ () => arrayRemove(i) }> 삭제 </button>
+							</li>
+							
+						)
+					} )
+				}
+			</ul>
+			<button onClick={ () => fruitNameReset() }>배열 첫번째 글자변경</button>
+			<button onClick={ () => fruitSort() }>배열 정렬</button>
+			<input type="text" id='fruitInput'/>
+			<button onClick={ () => arrayPush() }>배열추가</button>
+
+			{
+				isModal ? <Modal modalTitle={modalTitle}/> : null
+			}
+		</>
+	)
+}
+
+function Modal (props) {
+	return (
+		<>
+			<ul>
+				<li className='title'>{props.modalTitle}</li>
+			</ul>
+		</>
+	)
 }
 
 export default App;
