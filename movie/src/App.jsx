ad4d2297df4cbd378166791c9f74d27716188c35
조윤후 <showil20@sayinfo.co.kr>
2025-05-30 10:51:23 +0900

    커밋밋

--- movie/src/App.jsx
+++ movie/src/App.jsx
@@ -1,4 +1,4 @@
-import { useEffect, useState } from "react";
+import { useCallback, useEffect, useState } from "react";
 import Header from './components/Header'
 import Main from "./components/Main";
 import Detail from "./components/Detail";
